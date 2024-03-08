1bd612a843bd15aed7da604b2ca972f24ef50bca
Pubordee Leechitcham <pooh3441@gmail.com>
2024-03-08 17:19:16 +0700

    init theme / route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
+import { useRoutes } from "react-router-dom"
+import ThemeProvider from "./theme/themeProvider"
 
 function App() {
-
+  const elements = useRoutes([{
+      element: (
+        <Outlet />
+      ),
+      children: [
+        { path: '/', element: <h1>Home</h1> },
+      ]
+    },
+    { path: '/login', element: <h1>Login</h1> },
+    { path: '/register', element: <h1>Register</h1> },
+    { path: '*', element: <h1>404 Not Found</h1> },
+  ])
   return (
-    <h1>Pooh</h1>
-  )
+    <ThemeProvider>
+      {elements}
+    </ThemeProvider>
+  );
 }
 
 export default App
